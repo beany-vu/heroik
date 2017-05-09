04d9905bcd351bca519f052119134365b45addab
beany <vuhoang.hanu@gmail.com>
2017-05-09 21:33:51 +0700

    add slick

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
         wiredep: {
             task: {
-                src: 'index.html'
+                src: '*.html'
             }
         }, // wiredep
         sass: {
