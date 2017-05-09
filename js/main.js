2af91fbbd38d2fcba7c912cb7c6be9886ce754de
Dung Trinh <dungtrnhdnh@gmail.com>
2017-05-09 19:13:14 +0700

    update link button

--- js/main.js
+++ js/main.js
@@ -3,10 +3,16 @@
  */
 
 jQuery(document).ready(function () {
-    jQuery('.page-index .block-featured-list li').click(function () {
+    jQuery('.page-index .block-featured-list li.item').click(function () {
         jQuery(this).parent().find('.img-wrapper').removeClass('active');
         jQuery(this).find('.img-wrapper').addClass('active');
-        console.log(jQuery(this));
         jQuery(this).find('input').prop("checked", true);
+    });
+
+    jQuery('.page-business .block-featured-list li.item button').click(function (event) {
+        event.preventDefault();
+        jQuery(this).parent().parent().parent().find('li.item').removeClass('active');
+        jQuery(this).parent().parent().addClass('active');
+        jQuery(this).prev('input').prop("checked", true);
     });
 });
